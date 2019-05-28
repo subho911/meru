5d3deb8483342835861eb611c4c1a0502290b848
SU272344 <subhadip.debnath@wipro.com>
2019-05-28 19:27:20 +0530

    Hystrix added

--- client-service/src/main/java/com/meru/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/meru/clientservice/ClientServiceApplication.java
@@ -3,10 +3,12 @@ package com.meru.clientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
 @RibbonClient(name = "client-service", configuration = RibbonConfiguration.class)
 public class ClientServiceApplication {
 
--- client-service/src/main/java/com/meru/clientservice/controller/PriceClientController.java
+++ client-service/src/main/java/com/meru/clientservice/controller/PriceClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.meru.clientservice.entity.Price;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class PriceClientController {
@@ -26,8 +27,9 @@ public class PriceClientController {
 	@Autowired
 	RestTemplate restTemplate;
 	
-	//Ribbon through Eureka
+	//Ribbon and Hystrix through Eureka
 	@RequestMapping("/client/ribbon/price")
+	@HystrixCommand(fallbackMethod = "fallback_getAllPrice")
 	public List<Price> getAllPrice() {
 		ResponseEntity<List<Price>> response = restTemplate.exchange("http://price-service/price", HttpMethod.GET,
 				  null,
@@ -36,4 +38,8 @@ public class PriceClientController {
 		return price;
 	}
 
+	
+	public String fallback_getAllPrice() {
+		return "Service unavilable. Hello from Hystrix fallback method";
+	}
 }
